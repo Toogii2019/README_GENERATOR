c72896f24ff3ccdf89d9caad648b7f8d50cd3024
Toogii <dashdava@uw.edu>
2020-08-16 23:23:04 -0700

    90%

---
+++ README.md
@@ -0,0 +1,54 @@
+# Readme file generator
+
+## Description 
+
+Generates readme file for github repository
+
+![Github license](https://img.shields.io/badge/license-None-blue.svg)
+
+## Table of Content 
+
+ * [Installation](#installation) 
+
+ * [Usage](#usage) 
+
+ * [License](#license) 
+
+ * [Contributing](#contributing) 
+
+ * [tests](#tests) 
+
+* [Questions](#questions) 
+
+
+## Installation 
+
+To install necessery dependencies, run the following command:  
+
+```npm i --save``` 
+
+## Usage 
+
+n/a
+
+## License 
+
+ This project licensed under None license
+
+
+## Contributing 
+
+ n/a
+
+## Tests 
+
+To run tests, run the following command: 
+
+```npm test```
+
+## Questions 
+
+if you have any questions about the repo, open an issue or contact me directly at *****@uw.edu. You can find more of my work at [Toogii2019](https://github.com/Toogii2019) 
+
+
+
--- index.js
+++ index.js
@@ -1,16 +1,67 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+var generateMarkdown = require("./lib/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
-
+    {message: 'What is your github username ?',name: 'github_username', type: 'input'},
+    {message: 'What is your email address ?',name: 'email_address', type: 'input'},
+    {message: 'What is your project name ?',name: 'title', type: 'input'},
+    {message: 'Short description of your project: ',name: 'description', type: 'input'},
+    {message: 'What kind of license should your project have ?',name: 'license', type: 'list', choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', "None"]},
+    {message: 'What command should be run to install dependencies ?',name: 'cmd_dependency', type: 'input'},
+    {message: 'What command should be run to run tests ?',name: 'cmd_test', type: 'input'},
+    {message: 'What does the user need to know about using the repo ?',name: 'repo_use', type: 'input'},
+    {message: 'What does the user need to know about contributing to the repo ?',name: 'repo_contrib', type: 'input'},
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, dataArray) {
+    // Check if file exist !
+
+    for (var i=1;i<11;i++) {
+        var data = dataArray[i];
+        fs.appendFile(fileName, data + '\n\n', function(err) {
+            if (err) {
+                console.log(err);
+                }
+        });
+    }
+    console.log(`Successfully Generated file at ${fileName}`);
 }
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt(questions)
+    .then(function(response) {
+        console.log(response);
 
+        //Markdown called
+
+        let data = generateMarkdown(response);
+
+        if (fs.existsSync("./README/README.md")) {
+            console.log("File already exist");
+            fs.unlinkSync("./README/README.md");
+        }
+        else {
+
+
+            fs.mkdir("./README", function(err) {
+                if (err.code === "EEXIST") {
+                return;
+                } else {
+                console.log("New directory successfully created.")
+                }
+            })
+        }
+
+        writeToFile('./README/README.md', data);
+});
 }
+
+
 
 // function call to initialize program
 init();
